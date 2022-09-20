15d9d9a5f6d05b80bdaf1d4688b82a5ff7a4af77
yuanfengli168 <49966978+yuanfengli168@users.noreply.github.com>
2022-09-19 22:49:30 -0500

    lesson-5: Created Entity and Customer object.

--- onlineOrder/src/main/java/com/laioffer/onlineorder/HelloServlet.java
+++ onlineOrder/src/main/java/com/laioffer/onlineorder/HelloServlet.java
@@ -1,10 +1,12 @@
 package com.laioffer.onlineorder;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
@@ -20,6 +22,21 @@ public class HelloServlet extends HttpServlet {
         customer.put("email", "sun@laioffer.com");
         customer.put("first_name", "rick");
         response.getWriter().print(customer);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Read customer information from request body.
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String email = jsonRequest.getString("email");
+
+        // print customer information to IDE console
+        System.out.println("Email is: " + email);
+
+        // return status = ok as response body to the client
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().print(jsonResponse);
     }
 
     public void destroy() {
