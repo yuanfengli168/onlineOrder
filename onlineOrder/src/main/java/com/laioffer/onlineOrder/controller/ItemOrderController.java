70e76f63b62bd824b4f3225331ccd525cfb306f5
yuanfengli168 <49966978+yuanfengli168@users.noreply.github.com>
2022-10-08 22:05:27 -0700

    lesson-16: All backend worked

--- onlineOrder/src/main/java/com/laioffer/onlineOrder/controller/ItemOrderController.java
+++ onlineOrder/src/main/java/com/laioffer/onlineOrder/controller/ItemOrderController.java
@@ -1,5 +1,7 @@
 package com.laioffer.onlineOrder.controller;
 
+import com.laioffer.onlineOrder.service.OrderItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ItemOrderController {
+
+    @Autowired
+    private OrderItemService orderItemService;
+
     @RequestMapping(value = "/order/{menuId}", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public void addMenuItemToCart(@PathVariable("menuId") int menuId) {
-
+        orderItemService.saveOrderItem(menuId);
     }
 }
